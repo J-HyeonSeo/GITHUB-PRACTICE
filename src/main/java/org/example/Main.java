d9cb19a7fd6ddc7c6a3153bcc2076b27df26188a
J-HyeonSeo <47245112+J-HyeonSeo@users.noreply.github.com>
2023-08-22 07:49:15 +0900

    UPDATED MAIN BRANCH

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("MAIN BRANCH");
+    System.out.println("MAIN BRANCH UPDATED");
   }
 }
